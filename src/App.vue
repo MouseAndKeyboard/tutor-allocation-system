5db0f004cc3930651c651a9452fd6d68fb281319
Alan Cheng <alan.cheng24@gmail.com>
2022-09-17 14:12:46 +0800

    added form text input

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,19 @@
 
 <template>
   <main>
-    <h1 className="text-xl font-bold">Hello</h1>
+    <h1 className="text-xl font-bold">Tutor Allocation System</h1>
+    <h2>Enter Assessment Dates</h2>
+    <form>
+      <label>
+        Name:
+        <input type="text" name="name" />
+      </label>
+    </form>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+input {
+  border: 1px solid black;
+}
+</style>
