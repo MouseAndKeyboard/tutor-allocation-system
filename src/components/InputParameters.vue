da40712de9b7fda5b08881141c1fc7c3992b737c
Alan Cheng <alan.cheng24@gmail.com>
2022-09-17 16:37:27 +0800

    added number inputs for question rate

--- src/components/InputParameters.vue
+++ src/components/InputParameters.vue
@@ -1,6 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      questionsPerMinuteNoAssessments: 0,
+      questionsPerMinuteAssessments: 0,
+    };
+  },
+};
+</script>
+
 <template>
   <h2 className="text-2xl font-bold">Parameters</h2>
   <form>
-    <vue-number-input controls></vue-number-input>
+    <label>
+      Questions per minute (no assessments):
+      <vue-number-input
+        v-model="questionsPerMinuteNoAssessments"
+        controls
+      ></vue-number-input>
+    </label>
+    <label>
+      Questions per minute (assessments):
+      <vue-number-input
+        v-model="questionsPerMinuteAssessments"
+        controls
+      ></vue-number-input>
+    </label>
   </form>
 </template>
