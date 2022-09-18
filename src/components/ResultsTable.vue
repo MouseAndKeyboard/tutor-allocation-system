2781b0d77a63bbaf007a1dbfae8c9bc148cdf0b3
Alan Cheng <alan.cheng24@gmail.com>
2022-09-18 16:08:56 +0800

    fixed table headers

--- src/components/ResultsTable.vue
+++ src/components/ResultsTable.vue
@@ -6,17 +6,20 @@
       <thead>
         <tr>
           <th>Week</th>
-          <th>Number of Tutors</th>
-          <th>Max. Wait Time</th>
-          <th>Av. Wait Time</th>
-          <th>Money Spent</th>
+          <th>Assignment</th>
+          <th>No. of Tutors</th>
+          <th>No. of Labs</th>
+          <th>Total Wait Time</th>
+          <th>Total Wages Cost</th>
+          <th>Total Delay Cost</th>
+          <th>Total Overall Cost</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="num in Array.from({ length: 12 }, (_, i) => i + 1)">
-          {{
-            num
-          }}
+          <td>
+            {{ num }}
+          </td>
         </tr>
       </tbody>
     </table>
