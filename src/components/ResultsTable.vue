5df23f06a16d3b7de08763395933e0d4a794c999
Alan Cheng <alan.cheng24@gmail.com>
2022-09-18 17:52:42 +0800

    better table styling

--- src/components/ResultsTable.vue
+++ src/components/ResultsTable.vue
@@ -1,3 +1,39 @@
+<template>
+  <div class="my-5 w-screen">
+    <h2 class="font-bold mb-4 text-2xl">Results</h2>
+    <table
+      className="border-collapse border border-slate-400 m-auto text-center w-5/6"
+    >
+      <thead>
+        <tr>
+          <th
+            class="border border-gray-400"
+            v-for="header in [
+              'Week',
+              'Assignment',
+              'No. of Tutors',
+              'No. of Labs',
+              'Total Wait Time',
+              'Total Wages Cost',
+              'Total Delay Cost',
+              'Total Overall Cost',
+            ]"
+          >
+            {{ header }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="row in results">
+          <td class="border border-slate-400" v-for="val in row">
+            {{ val }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
 <script>
 import optimal_allocations from "../utils/otpimal_allocations";
 const results = optimal_allocations(
@@ -22,8 +58,10 @@ const results = optimal_allocations(
   120
 );
 for (const row of results) {
+  row[1] = row[1] ? "T" : "F";
   row[4] = row[4].toFixed(0);
   row[6] = row[6].toFixed(2);
+  row.push(parseFloat(row[5]) + parseFloat(row[6]));
 }
 
 export default {
@@ -59,38 +97,5 @@ export default {
   },
 };
 </script>
-
-<template>
-  <div>
-    <table className="border-collapse border border-slate-400 text-center">
-      <thead>
-        <tr>
-          <th
-            class="border border-gray-400"
-            v-for="header in [
-              'Week',
-              'Assignment',
-              'No. of Tutors',
-              'No. of Labs',
-              'Total Wait Time',
-              'Total Wages Cost',
-              'Total Delay Cost',
-              'Total Overall Cost',
-            ]"
-          >
-            {{ header }}
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="row in results">
-          <td class="border border-slate-400" v-for="val in row">
-            {{ val }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</template>
 
 <style scoped></style>
