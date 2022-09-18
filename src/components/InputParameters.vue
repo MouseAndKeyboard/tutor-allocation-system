dec90237fff47e554dc5f1c2b8f7af422e2a7e0c
Alan Cheng <alan.cheng24@gmail.com>
2022-09-18 14:34:00 +0800

    added more params

--- src/components/InputParameters.vue
+++ src/components/InputParameters.vue
@@ -1,11 +1,15 @@
 <script>
+import { reactive } from "vue";
+
 export default {
   data() {
     return {
       avgTutorHelpTime: 0,
       labDuration: 120,
+      labsPerWeek: 0,
       questionsPerMinuteNoAssessments: 0,
       questionsPerMinuteAssessments: 0,
+      tutorSalary: 50,
     };
   },
 };
@@ -32,6 +36,10 @@ export default {
     <input type="number" min="0" step=".01" v-model="avgTutorHelpTime" />
     <label>Lab duration (mins):</label>
     <input type="number" min="0" v-model="labDuration" />
+    <label>Labs per week:</label>
+    <input type="number" min="0" v-model="labsPerWeek" />
+    <label>Tutor salary (per hour):</label>
+    <input type="number" min="0" v-model="tutorSalary" />
   </form>
 </template>
 
