456149d2dd0cdfbd2480a9f64d05fa7de2f8e5a1
Michael Nef <michael@nef.net.au>
2022-10-02 12:58:24 +0800

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  "base": "/tutor-allocation-system/",
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./tutor-allocation-system/src/', import.meta.url))
     }
   },
-  base: "/tutor-allocation-system/"
+  
 })
+
